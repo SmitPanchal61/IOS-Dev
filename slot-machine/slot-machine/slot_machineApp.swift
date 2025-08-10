58f5c1aabdf1784ba59dfcb822cb4033bcab1769
Smit panchal <90374408+SmitPanchal61@users.noreply.github.com>
2025-08-10 12:50:04 -0700

    Added Files for Slot Machine App

---
+++ slot-machine/slot-machine/slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machineApp.swift
+//  slot-machine
+//
+//  Created by Smit Mahesh Panchal on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ slot-machine/slot-machineTests/slot_machineTests.swift
@@ -0,0 +1,16 @@
+//
+//  slot_machineTests.swift
+//  slot-machineTests
+//
+//  Created by Smit Mahesh Panchal on 6/8/25.
+//
+
+import Testing
+
+struct slot_machineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
